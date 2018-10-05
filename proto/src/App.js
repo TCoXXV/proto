8db0e09105b4cb4977f5fa36749001fe3c47d6cf
TCoXXV <tccodingxxv@gmail.com>
2018-10-05 13:14:33 -0700

    v1.2

---
+++ proto/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <body>
+          <NavBar />
+      </body>    
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ proto/src/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './NavBar.scss';
+
+export default class NavBar extends Component {
+    
+
+    render(){
+        return (
+            <div id='nav1'>
+                <header>
+                    <div >
+                        <span className="hhome" >Home</span>
+                        <span className='htitle' >Prototype Site</span>
+                        <span className='rcontact'>Contact</span>
+                    </div>
+                </header>
+
+            
+            
+            </div>
+            
+        )
+    }
+}
